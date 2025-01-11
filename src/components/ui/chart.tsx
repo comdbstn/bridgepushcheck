f0f4e38f5da69014de8ae17167954b2044b7591a
tachyon6 <songjunjun62754@gmail.com>
2025-01-12 00:40:32 +0900

    fix: Remove unused imports in chart component

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
-import { NameType, Payload, ValueType } from "recharts/types/component/DefaultTooltipContent";
+import type { TooltipProps } from "recharts";
 
 import { cn } from "@/lib/utils";
 
